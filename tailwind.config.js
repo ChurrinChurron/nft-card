35d9907681a95d816b23fa7aa5e459d72b836828
Ana <marianafantasy8@gmail.com>
2021-11-28 22:25:11 -0400

    proyecto-nft-card

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    	colors: {
+    		softblue: "hsl(215, 51%, 70%)",
+    		cyan: "hsl(178, 100%, 50%)",
+    		darkbluemain: "hsl(217, 54%, 11%)",
+    		darkbluecard: "hsl(216, 50%, 16%)",
+    		darkblueline: "hsl(215, 32%, 27%)",
+    		},
+    	},
+
+    	fontFamily: {
+      	out: ['Outfit'],
+    	},
+  	},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
